e3a93bc15500411d8b339b9bb2ee3a42f858d52b
zolamars <zolyemek@gmail.com>
2022-06-06 17:11:10 +0300

    Use `@reach/dialog`

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,18 +1,33 @@
+import '@reach/dialog/styles.css'
 import React from "react";
 import { Logo } from "components/logo";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import {Dialog} from '@reach/dialog'
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none');
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login clicked!')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('register clicked!')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label='login-modal' isOpen={openModal === 'login'} >
+        <div>
+          <button onClick={()=> setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label='register-modal' isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={()=> setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </div>
   )
 }
