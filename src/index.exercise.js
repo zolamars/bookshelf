aff760ec12124e7471e995e129dc9b3d7b2ed50b
zolamars <zolyemek@gmail.com>
2022-06-06 16:24:58 +0300

    Formatted the document

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Logo } from "components/logo";
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
- function App(){     
-     return(
-        <div>
-        <Logo width="80" height="80"/>
-        <h1>Bookshelf</h1>
-        <div>
-          <button onClick={()=> alert('login clicked!')}>Login</button>
-        </div>
-        <div>
-          <button onClick={()=> alert('register clicked!')}>Register</button>
-        </div>
+function App() {
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => alert('login clicked!')}>Login</button>
       </div>
-     )
- }
+      <div>
+        <button onClick={() => alert('register clicked!')}>Register</button>
+      </div>
+    </div>
+  )
+}
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
-export {root}
+export { root }
