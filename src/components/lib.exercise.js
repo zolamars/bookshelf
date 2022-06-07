a57f63d65fddb7401d86d10f18269e64496bffd8
zolamars <zolyemek@gmail.com>
2022-06-07 13:59:53 +0300

    use the emotion macro

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,34 +1,31 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
 
 const buttonVariants = {
-  primary: { 
+  primary: {
     background: '#3f51b5',
     color: 'white',
   },
   secondary: {
     background: '#f1f2f7',
     color: '#434449',
-  }
+  },
 }
-const Button = styled.button({
-  padding: '10px 15px',
+const Button = styled.button(
+  {
+    padding: '10px 15px',
     border: '0',
     lineHeight: '1',
     borderRadius: '3px',
   },
-  ({variant = 'primary'})=> buttonVariants[variant]
+  ({variant = 'primary'}) => buttonVariants[variant],
 )
+
 const Input = styled.input({
   borderRadius: '3px',
   border: '1px solid #f1f1f4',
   background: '#f1f2f7',
   padding: '8px 12px',
-})
-
-const FormGroup = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
 })
 
 const CircleButton = styled.button({
@@ -58,4 +55,9 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-export {Button, CircleButton, Dialog, FormGroup, Input}
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export {Button, Input, CircleButton, Dialog, FormGroup}
--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
 import 'bootstrap/dist/css/bootstrap-reboot.css'
 import '@reach/dialog/styles.css'
 import * as React from 'react'
@@ -18,18 +21,18 @@ function LoginForm({onSubmit, submitButton}) {
   }
 
   return (
-    <form 
-    css={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'stretch',
-      '> div': {
-        margin: '10px auto',
-        width: '100%',
-        maxWidth: '300px',
-      },
-    }}
-    onSubmit={handleSubmit}
+    <form
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        '> div': {
+          margin: '10px auto',
+          width: '100%',
+          maxWidth: '300px',
+        },
+      }}
+      onSubmit={handleSubmit}
     >
       <FormGroup>
         <label htmlFor="username">Username</label>
@@ -54,22 +57,24 @@ function App() {
   }
 
   return (
-    <div css={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-      height: '100vh',
-    }}
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
     >
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      <div css={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
-        gridGap: '0.75rem',
-      }}
+      <div
+        css={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+          gridGap: '0.75rem',
+        }}
       >
         <Modal>
           <ModalOpenButton>
