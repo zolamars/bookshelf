46879b066a4e8a77e7bcac06146de69e8182ef49
zolamars <zolyemek@gmail.com>
2022-06-07 14:11:17 +0300

    use colors and media queries file

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,14 +1,16 @@
 import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
+import * as colors from 'styles/colors'
+import * as mq from 'styles/media-queries'
 
 const buttonVariants = {
   primary: {
-    background: '#3f51b5',
-    color: 'white',
+    background: colors.indigo,
+    color: colors.base,
   },
   secondary: {
-    background: '#f1f2f7',
-    color: '#434449',
+    background: colors.gray,
+    color: colors.text,
   },
 }
 const Button = styled.button(
@@ -23,8 +25,8 @@ const Button = styled.button(
 
 const Input = styled.input({
   borderRadius: '3px',
-  border: '1px solid #f1f1f4',
-  background: '#f1f2f7',
+  border: `1px solid ${colors.gray10}`,
+  background: colors.gray,
   padding: '8px 12px',
 })
 
@@ -37,9 +39,9 @@ const CircleButton = styled.button({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
   cursor: 'pointer',
 })
 
@@ -49,7 +51,7 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: '3.5em',
   boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
   margin: '20vh auto',
-  '@media (max-width: 991px)': {
+  [mq.small]: {
     width: '100%',
     margin: '10vh auto',
   },
